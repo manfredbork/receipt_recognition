078d9238966f72b1d084033927525615ad912084
Manfred Bork <bork.ma@t-online.de>
2025-05-07 11:29:39 +0200

    Added zigzag edge to view

--- example/lib/views/receipt_widget.dart
+++ example/lib/views/receipt_widget.dart
@@ -11,23 +11,16 @@ class ReceiptWidget extends StatelessWidget {
     return CustomScrollView(
       slivers: <Widget>[
         SliverPadding(
-          padding: const EdgeInsets.all(20.0),
+          padding: const EdgeInsets.symmetric(vertical: 40.0, horizontal: 30.0),
           sliver: SliverList(
             delegate: SliverChildListDelegate(<Widget>[
+              ZigzagEdgeWidget(isTop: true),
               Container(
                 padding: EdgeInsets.all(16),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(12),
-                  border: Border.all(
-                    color: Colors.black.withValues(alpha: 0.1),
-                    width: 1,
-                  ),
-                ),
+                decoration: BoxDecoration(color: Colors.white),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    SizedBox(height: 32),
                     Text(
                       receipt.company?.formattedValue ?? 'Unknown Store',
                       style: TextStyle(
@@ -88,10 +81,78 @@ class ReceiptWidget extends StatelessWidget {
                   ],
                 ),
               ),
+              ZigzagEdgeWidget(isTop: false),
             ]),
           ),
         ),
       ],
     );
   }
+}
+
+class ZigzagEdgeWidget extends StatelessWidget {
+  final bool isTop;
+  final double zigzagWidth;
+  final double zigzagHeight;
+  final Color triangleColor;
+  final double height;
+
+  const ZigzagEdgeWidget({
+    super.key,
+    this.isTop = false,
+    this.zigzagWidth = 5.0,
+    this.zigzagHeight = 5.0,
+    this.triangleColor = Colors.white,
+    this.height = 5.0,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomPaint(
+      painter: _ZigzagEdgePainter(
+        isTop: isTop,
+        zigzagWidth: zigzagWidth,
+        zigzagHeight: zigzagHeight,
+        triangleColor: triangleColor,
+      ),
+      child: SizedBox(height: height),
+    );
+  }
+}
+
+class _ZigzagEdgePainter extends CustomPainter {
+  final bool isTop;
+  final double zigzagWidth;
+  final double zigzagHeight;
+  final Color triangleColor;
+
+  _ZigzagEdgePainter({
+    required this.isTop,
+    required this.zigzagWidth,
+    required this.zigzagHeight,
+    required this.triangleColor,
+  });
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final trianglePaint = Paint()..color = triangleColor;
+
+    for (double x = 0; x < size.width; x += zigzagWidth) {
+      final trianglePath = Path();
+      if (isTop) {
+        trianglePath.moveTo(x, size.height);
+        trianglePath.lineTo(x + zigzagWidth / 2, size.height - zigzagHeight);
+        trianglePath.lineTo(x + zigzagWidth, size.height);
+      } else {
+        trianglePath.moveTo(x, 0);
+        trianglePath.lineTo(x + zigzagWidth / 2, zigzagHeight);
+        trianglePath.lineTo(x + zigzagWidth, 0);
+      }
+      trianglePath.close();
+      canvas.drawPath(trianglePath, trianglePaint);
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
 }
