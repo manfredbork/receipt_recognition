3aee42f475cd35b7e9f4a426441975db142e5750
Manfred Bork <bork.ma@t-online.de>
2025-05-09 18:57:50 +0200

    Debug messages

--- lib/src/receipt_optimizer.dart
+++ lib/src/receipt_optimizer.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/foundation.dart';
+
 import 'receipt_models.dart';
 
 class ReceiptOptimizer implements Optimizer {
@@ -26,6 +28,20 @@ class ReceiptOptimizer implements Optimizer {
 
     _cachedReceipt.apply(receipt);
     _cachedReceipt.merge();
+
+    if (kDebugMode) {
+      if (_cachedReceipt.receipt.positions.isNotEmpty) {
+        print('####################################');
+      }
+      for (final position in _cachedReceipt.receipt.positions) {
+        print(
+          '${position.product.value} ${position.price.formattedValue} ${position.trustworthiness}',
+        );
+      }
+      if (_cachedReceipt.receipt.positions.isNotEmpty) {
+        print(_cachedReceipt.receipt.calculatedSum.formattedValue);
+      }
+    }
 
     if (_cachedReceipt.isCorrectSum && _cachedReceipt.areMinScansReached) {
       _cachedReceipt.isValid = true;
