4fb07f7ed5b4e82820c269def50729b2036e215e
Manfred Bork <bork.ma@t-online.de>
2025-06-03 12:54:54 +0200

    Added comments

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,17 +1,23 @@
 import 'package:example/views/receipt_recognition_view.dart';
 import 'package:flutter/material.dart';
 
+/// Entry point of the Receipt Recognition example app.
 void main() {
   runApp(const ExampleApp());
 }
 
+/// The root widget of the Receipt Recognition example application.
+///
+/// Sets up a basic [MaterialApp] with theming and launches the
+/// [ReceiptRecognitionView] as the home screen.
 class ExampleApp extends StatelessWidget {
+  /// Creates an instance of [ExampleApp].
   const ExampleApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Example Receipt Recognition',
+      title: 'Receipt Recognition',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightGreen),
