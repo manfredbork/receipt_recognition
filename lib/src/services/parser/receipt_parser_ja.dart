96ae5c63d569c3c5e146f0c588ba6c9df3211a07
manfredbork <bork.ma@t-online.de>
2026-04-15 12:58:54 +0200

    app: added import to fix flutter analyze issue

--- lib/src/services/parser/receipt_parser_ja.dart
+++ lib/src/services/parser/receipt_parser_ja.dart
@@ -3,6 +3,7 @@ import 'dart:ui';
 import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
 import 'package:receipt_recognition/src/models/index.dart';
 import 'package:receipt_recognition/src/services/ocr/index.dart';
+import 'package:receipt_recognition/src/services/parser/receipt_parser.dart';
 import 'package:receipt_recognition/src/utils/configuration/index.dart';
 import 'package:receipt_recognition/src/utils/normalize/index.dart';
 
